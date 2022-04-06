acfa3f1171d809c8d0d8e3e562ccd0a0d336115a
WaiMaungMaung <mgwaimaungmaung@gmail.com>
2022-04-06 10:15:48 +0630

    first

--- .history/resources/views/welcome.blade_20220406100849.php
+++ .history/resources/views/welcome.blade_20220406100849.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 <div id="container">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Brand</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav">
